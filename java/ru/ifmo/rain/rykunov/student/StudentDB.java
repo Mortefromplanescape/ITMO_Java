e93867daeeed81eae580be25e309be197157b477
Nikolay Rykunov <Walingar@mail.ru>
2018-03-11 22:22:57 +0300

    Updated HW3

--- java/ru/ifmo/rain/rykunov/student/StudentDB.java
+++ java/ru/ifmo/rain/rykunov/student/StudentDB.java
@@ -4,9 +4,11 @@ import info.kgeorgiy.java.advanced.student.Student;
 import info.kgeorgiy.java.advanced.student.StudentQuery;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StudentDB implements StudentQuery {
 
@@ -17,77 +19,113 @@ public class StudentDB implements StudentQuery {
                     .thenComparingInt(Student::getId);
 
     private List<String> getStudentsParameters(List<Student> students, Function<Student, String> func) {
-        return students.parallelStream()
-                .map(func)
+        return getStudentsParametersStream(students, func)
                 .collect(Collectors.toList());
     }
 
+    private Stream<String> getStudentsParametersStream(List<Student> students, Function<Student, String> func) {
+        return students.stream()
+                .map(func);
+    }
+
+    @Override
     public List<String> getFirstNames(List<Student> students) {
         return getStudentsParameters(students, Student::getFirstName);
     }
 
+    @Override
     public List<String> getLastNames(List<Student> students) {
         return getStudentsParameters(students, Student::getLastName);
     }
 
+    @Override
     public List<String> getGroups(List<Student> students) {
         return getStudentsParameters(students, Student::getGroup);
     }
 
+    @Override
     public List<String> getFullNames(List<Student> students) {
-        return getStudentsParameters(students, s -> s.getFirstName() + " " + s.getLastName());
+        return getStudentsParameters(students, s -> String.format("%s %s", s.getFirstName(), s.getLastName()));
     }
 
+    @Override
     public Set<String> getDistinctFirstNames(List<Student> students) {
-        return students.parallelStream()
-                .map(Student::getFirstName)
-                .collect(Collectors.toSet());
+        return getStudentsParametersStream(students, Student::getFirstName)
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 
+    @Override
     public String getMinStudentFirstName(List<Student> students) {
-        return students.parallelStream()
+        return students.stream()
                 .min(Comparator.comparingInt(Student::getId))
-                .orElse(new Student(0, "", "", "0"))
-                // or .get() but unchecked empty List
-                .getFirstName();
+                .map(Student::getFirstName)
+                .orElse("");
     }
 
     private List<Student> sortedStudentsBy(Collection<Student> students, Comparator<Student> comparator) {
-        return students.parallelStream()
-                .sorted(comparator)
+        return getSortedStudentStreamFromCollection(students, comparator)
                 .collect(Collectors.toList());
     }
 
-    public List<Student> sortStudentsById(Collection<Student> students) {
-        return sortedStudentsBy(students, Comparator.comparingInt(Student::getId));
+    private Stream<Student> getSortedStudentStreamFromCollection(Collection<Student> students, Comparator<Student> comparator) {
+        return students.stream().sorted(comparator);
     }
 
+    private Stream<Student> getSortedStudentStreamFromStream(Stream<Student> students, Comparator<Student> comparator) {
+        return students.sorted(comparator);
+    }
+
+    private Stream<Student> getFilteredStudentStreamFromCollection(Collection<Student> students, Predicate<Student> pred) {
+        return students.stream().filter(pred);
+    }
+
+    @Override
+    public List<Student> sortStudentsById(Collection<Student> students) {
+        return sortedStudentsBy(students, Student::compareTo);
+    }
+
+    @Override
     public List<Student> sortStudentsByName(Collection<Student> students) {
         return sortedStudentsBy(students, studentsCompare);
     }
 
     private List<Student> filteredStudents(Collection<Student> students, Predicate<Student> predicate) {
-        return students.parallelStream()
-                .filter(predicate)
-                .sorted(studentsCompare)
+        return getSortedStudentStreamFromStream(
+                getFilteredStudentStreamFromCollection(students, predicate), studentsCompare)
                 .collect(Collectors.toList());
     }
 
+    private Predicate<Student> getFirstNamePredicate(String name) {
+        return s -> s.getFirstName().equals(name);
+    }
+
+    private Predicate<Student> getLastNamePredicate(String name) {
+        return s -> s.getLastName().equals(name);
+    }
+
+    private Predicate<Student> getGroupPredicate(String group) {
+        return s -> s.getGroup().equals(group);
+    }
+
+    @Override
     public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
-        return filteredStudents(students, s -> s.getFirstName().equals(name));
+        return filteredStudents(students, getFirstNamePredicate(name));
     }
 
+    @Override
     public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
-        return filteredStudents(students, s -> s.getLastName().equals(name));
+        return filteredStudents(students, getLastNamePredicate(name));
     }
 
+    @Override
     public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
-        return filteredStudents(students, s -> s.getGroup().equals(group));
+        return filteredStudents(students, getGroupPredicate(group));
     }
 
+    @Override
     public Map<String, String> findStudentNamesByGroup(Collection<Student> students, String group) {
-        return students.parallelStream()
-                .filter(s -> s.getGroup().equals(group))
-                .collect(Collectors.toMap(Student::getLastName, Student::getFirstName, (String s1, String s2) -> s1.compareTo(s2) < 0 ? s1 : s2));
+        return getFilteredStudentStreamFromCollection(students, getGroupPredicate(group))
+                .collect(Collectors.toMap(Student::getLastName, Student::getFirstName,
+                        BinaryOperator.minBy(String::compareTo)));
     }
 }
